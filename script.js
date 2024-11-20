c0b6b97e7898e750d2dec3ea05164cff69beede5
Zenku006 <playeralex2006@gmail.com>
2024-11-20 23:52:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+//let a = 25;
+//let b = 35;
+//const c = a+b;
+//alert(c);
+//console.log(c);
+//let name= "Антон";
+//let text = "Привіт";
+//console.log(text+name);
+//alert(text+name);
+
+// Домашня робота з 4.1
+// let myYear = 2006;
+// let lastYear = 2024;
+// let difference = lastYear - myYear;
+// alert(difference);
+
+//let a = 15;
+//let b = 25;
+//console.log(a > b);
+// const userPassword = prompt('Введіть пароль:');
+// validPassword = '1234';
+// if (userPassword == validPassword){
+//     alert('Доступ надано');
+// } else{
+//     alert('Невірний пароль');
+// }
+
+// let profession = prompt("Вкажи свою професію");
+
+// if (profession == 'Лікар') {
+// alert('Дякуємо, що лікуєте.');
+// } else if (profession == 'Пожежник') {
+// alert('Дякуємо, що рятуєте.');
+// } else if (profession == 'Пекар') {
+// alert('Дякуємо, що годуєте.');
+// } else if (profession == 'Водій') {
+// alert('Дякуємо, що возите.');
+// } else {
+// alert('Я знаю багато професій');
+// }
+
+// let a = 10;
+// let b = 15;
+// let c = 25;
+
+// if(a<b || a<c){
+//     console.log("Число a менше за b або c");
+// } else{
+//     console.log("Умова не виконується");
+// }
+// console.log(10!=15);
+// let f = 35;
+// if(a<b && a<f){
+//     console.log("Число a менше за b або f")
+// }else{
+//     console.log("Умова не виконується")
+// }
+
+
+//Домашня робота з 4.2
+let season = prompt("Вкажи номер сезону");
+
+if(season == 1){
+    alert('Це весна');
+    console.log('Це весна');
+} else if (season == 2){
+    alert('Це літо');
+    console.log('Це літо');
+} else if (season == 3){
+    alert('Це осінь');
+    console.log('Це осінь');
+} else if (season == 4){
+    alert('Це зима');
+    console.log('Це зима');
+} else {
+    alert('Такого сезону не існує');
+    console.log('Такого сезону не існує');
+}
